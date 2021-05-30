70618c33556c2ffb338e38112fdbdfde2f52b1af
Camilla Pantoja <pantojacamilla@gmail.com>
2021-05-29 21:48:23 -0300

    Alinha os includes de acordo com a norminette

--- libft/libft.h
+++ libft/libft.h
@@ -1,13 +1,9 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-// coloquei isso pq se não ele diz que desconhece o size_t
 # include <stdlib.h>
-#include <ctype.h>
+# include <ctype.h>
 
 size_t	ft_strlen(const char *s);
-int		ft_isdigit(int cs);
-
-
-
+int		ft_isdigit(int c);
 #endif
