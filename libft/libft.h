c574c900970024118e172e3378694bee80e85fb5
Camilla Pantoja <pantojacamilla@gmail.com>
2021-05-29 21:38:55 -0300

    Adiciona referencia a ft_isdigit

--- libft/libft.h
+++ libft/libft.h
@@ -1,9 +1,13 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-// coloquei isso pq se não ele diz que desconhece o  size_t
+// coloquei isso pq se não ele diz que desconhece o size_t
 # include <stdlib.h>
+#include <ctype.h>
 
 size_t	ft_strlen(const char *s);
+int		ft_isdigit(int cs);
+
+
 
 #endif
