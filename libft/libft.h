b546f84e25ecc2c801c0db155120858eff37211c
Camilla Pantoja <pantojacamilla@gmail.com>
2021-05-29 21:02:12 -0300

    Corrigi o arquivo Makefile

--- libft/libft.h
+++ libft/libft.h
@@ -1,6 +1,9 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-size_t	ft_strlen(const char *s);3
+// coloquei isso pq se não ele diz que desconhece o  size_t
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
 
 #endif
---
+++ libft/main.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	//printf("pokemon temos que pegar\n");
+	const char *s = "Oi mundo";
+	printf("%s", s);
+
+	printf("\n%zu\n", ft_strlen(s));
+	return (0);
+}
