957f51588deddeccf5c4fc7f3b6be3c43ca9649a
Camilla Pantoja <pantojacamilla@gmail.com>
2021-07-07 18:20:04 -0300

    Implementa o strlen

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,35 +1,54 @@
 #include "libft.h"
 #include <stdio.h>
+
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	/*
-		Basically copies l char from *src to *dst
-		If dstsize is > 0 cool
+	int amount_chars_src;
+	int current_position;
+	char *start;
 
-		Always returns the total length of the string they tried to create (src)
-	*/
-
-	int offset;
-
-	offset = 0;
+	current_position = 0;
+	amount_chars_src = 0;
+		*start = *src;
 	if (dstsize > 0)
-	{ // do what you have to do
+	{
+		while (*src != '\0' && current_position < (dstsize - 1))
+		{
+			*dst = *src;
+			dst++;
+			src++;
+			current_position++;
+		}
 
+		*dst = '\0';
 	}
 
-	// contar a qtd de caracteres de src representado por offset
+	while (*start != '\0')
+	{
+		start++;
+		amount_chars_src++;
+	}
+	printf("\n");
 
-	return (offset);
+	return (amount_chars_src);
 }
 // clang ft_strlcpy.c -o teste && ./teste
 int main(void)
 {
-	const char source[] = "Ola mundo";
-	char dest[] = "";
+	const char sourceOG[] = "AEIO U";
+	char destOG[] = "";
 
-	printf("%lu\n", strlcpy(dest, source, -2));
+	const char sourceMine[] = "AEIO U";
+	char destMine[] = "";
 
-	printf("%s\n", dest);
+
+	printf("OG OUTPUT: %lu\n", strlcpy(destOG, sourceOG, -1)); // para dstsize == 2 é passado para destino O e /null
+	printf("OG DESTINATION: %s\n", destOG);
+
+	printf("\n\n");
+
+	printf("MINEs OUTPUT: %lu\n", ft_strlcpy(destMine, sourceMine, -1)); // para dstsize == 2 é passado para destino O e \0
+	printf("MINEs DESTINATION: %s\n", destMine);
 
 	return (0);
 }
