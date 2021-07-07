3e2229026451d87be014815c3e5d24c9a4cad211
Camilla Pantoja <pantojacamilla@gmail.com>
2021-07-07 18:22:32 -0300

    Correções de acordo com a norma

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -3,13 +3,13 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	int amount_chars_src;
-	int current_position;
-	char *start;
+	int		amount_chars_src;
+	int		current_position;
+	char	*start;
 
 	current_position = 0;
 	amount_chars_src = 0;
-		*start = *src;
+	*start = *src;
 	if (dstsize > 0)
 	{
 		while (*src != '\0' && current_position < (dstsize - 1))
@@ -19,36 +19,12 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 			src++;
 			current_position++;
 		}
-
 		*dst = '\0';
 	}
-
 	while (*start != '\0')
 	{
 		start++;
 		amount_chars_src++;
 	}
-	printf("\n");
-
 	return (amount_chars_src);
-}
-// clang ft_strlcpy.c -o teste && ./teste
-int main(void)
-{
-	const char sourceOG[] = "AEIO U";
-	char destOG[] = "";
-
-	const char sourceMine[] = "AEIO U";
-	char destMine[] = "";
-
-
-	printf("OG OUTPUT: %lu\n", strlcpy(destOG, sourceOG, -1)); // para dstsize == 2 é passado para destino O e /null
-	printf("OG DESTINATION: %s\n", destOG);
-
-	printf("\n\n");
-
-	printf("MINEs OUTPUT: %lu\n", ft_strlcpy(destMine, sourceMine, -1)); // para dstsize == 2 é passado para destino O e \0
-	printf("MINEs DESTINATION: %s\n", destMine);
-
-	return (0);
 }
