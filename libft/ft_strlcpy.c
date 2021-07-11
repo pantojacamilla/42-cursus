a4a1b9dab4e35c5a9ba20a601f7ba0ea39579681
Camilla Pantoja <pantojacamilla@gmail.com>
2021-07-11 18:54:22 -0300

    Corrige o arquivo

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,5 +1,4 @@
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -1,9 +1,5 @@
 #include "libft.h"
 
-/*
-** ('a' - 'A') == 32
-*/
-
 int	ft_tolower(int c)
 {
 	if (c >= 'A' && c <= 'Z')
